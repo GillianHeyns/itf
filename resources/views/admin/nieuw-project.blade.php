944de2bb7b69eee03fd09478eda188d45215843b
Jonas Van Hove <r0741524@student.thomasmore.be>
2021-11-19 10:15:16 +0100

    frontend testimony

--- resources/views/admin/nieuw-project.blade.php
+++ resources/views/admin/nieuw-project.blade.php
@@ -13,16 +13,6 @@
                     <div class="card-body">
                         <form action="submit" method="post">
                             @csrf
-                            {{--                        <p>--}}
-                            {{--                            <label for="titel">Titel: </label>--}}
-                            {{--                            <input type="text" id="titel" name="titel">--}}
-                            {{--                        </p>--}}
-                            {{--                        <p>--}}
-                            {{--                            <label for="beschrijving">Beschrijving: </label>--}}
-                            {{--                            <input type="text" id="beschrijving" name="beschrijving">--}}
-                            {{--                        </p>--}}
-
-
                             <div class="form-group row">
                                 <label for="titel" class="col-md-4 col-form-label text-md-right">Titel: </label>
                                 <div class="col-md-6">
@@ -30,7 +20,6 @@
                                            class="form-control @error('titel') is-invalid @enderror">
                                 </div>
                             </div>
-
                             <div class="form-group row">
                                 <label for="beschrijving"
                                        class="col-md-4 col-form-label text-md-right">Beschrijving: </label>
@@ -39,8 +28,6 @@
                                            class="form-control @error('beschrijving') is-invalid @enderror">
                                 </div>
                             </div>
-
-
                             <div class="form-group row mb-0">
                                 <div class="col-md-4 offset-md-6">
                                     <button type="submit" class="col-md-12 btn btn-primary">
