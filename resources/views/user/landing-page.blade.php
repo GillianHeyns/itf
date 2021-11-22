49fea63697774576fe1bb32463bb6ae1cca77369
Jonas Van Hove <r0741524@student.thomasmore.be>
2021-11-22 12:17:50 +0100

    front-end landing-page

--- resources/views/user/landing-page.blade.php
+++ resources/views/user/landing-page.blade.php
@@ -7,28 +7,16 @@
 @section('main')
     <div class="text-center">
         <div class="m-5"></div>
-        <h1 class="display-4 mt-4 mb-4">Welkom {{ auth()->user()->name }}!</h1>
+        <h1 class="display-4 mt-4 mb-4" style="font-family: Nunito;">Welkom {{ auth()->user()->name }}!</h1>
         <div class="m-5"></div>
 
         <div class="row justify-content-around">
-            <a style="color: black; text-decoration: none;"
-               style="color: black; text-decoration: none;" asp-area="" asp-controller="Unit" asp-action="Index">
+            <a href="/admin/cms" style="color: black; text-decoration: none;" >
                 <div class="card" style="width: 18rem;">
-                    <img class="card-img-top" src="~/img/products.jpg" alt="Card image products">
+                    <img class="card-img-top" src="assets/img/itfactory_interactive/ITF_visual_new.jpg" alt="Card image CMS">
                     <div class="card-body">
-                        <h5 class="card-title">Producten</h5>
-                        <p class="card-text">Een lijst van alle producten</p>
-                    </div>
-                </div>
-            </a>
-            <a style="color: black; text-decoration: none;"
-               style="color: black; text-decoration: none;" asp-area="" asp-controller="Supplier" asp-action="Index">
-                <div class="card" style="width: 18rem;">
-                    <img class="card-img-top" src="~/img/suppliers.jpg" alt="Card image suppliers">
-                    <div class="card-body">
-                        <h5 class="card-title">CMS</h5>
+                        <h5 class="card-title text-bold">CMS</h5>
                         <p class="card-text">Beheer van projecten, getuigenissen, tags, etc.</p>
-
                     </div>
                 </div>
             </a>
