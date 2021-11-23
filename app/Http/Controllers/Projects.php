c8ee1a3d3c567c8a2fa1b2f3ce20bce69a23eb70
gillian_heyns <r0450988@student.thomasmore.be>
2021-11-23 11:15:09 +0100

    meerdere foto's

--- app/Http/Controllers/Projects.php
+++ app/Http/Controllers/Projects.php
@@ -13,13 +13,22 @@ class Projects extends Controller
             'file'=>'required|image|mimes:jpeg,png,pg,gif,svg|max:2048',
         ]);
 
-        $imageName=time().'.'.$req->file('file')->extension();
-        $req->file('file')->move(public_path('uploads/projects'),$imageName);
+//        $imageName=time().'.'.$req->file('file')->extension();
+//        $req->file('file')->move(public_path('uploads/projects'),$imageName);
+
+        $photo=array();
+
+        foreach ($req->file('file') as $file){
+            $imageName = time() . '.' . $file->extension();
+            $file->move(public_path('uploads/projects'),$imageName);
+            $photo[]=$imageName;
+        }
 
         $project = new Project;
         $project->titel= $req->titel;
         $project->beschrijving= $req->beschrijving;
-        $project->file_path='/storage/uploads/projects/'.$imageName;
+        //$project->file_path='/storage/uploads/projects/'.$imageName;
+        $project->file_path=json_encode($photo);
         $project->save();
 
         $data= DB::table('projects')->get();
