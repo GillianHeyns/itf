fbd287ac28205a1d64c9db0473d5a32987e71c89
Jonas Van Hove <r0741524@student.thomasmore.be>
2021-11-29 15:55:53 +0100

    testimony data & delete

--- database/migrations/2021_11_17_093240_create_testimonies_table.php
+++ database/migrations/2021_11_17_093240_create_testimonies_table.php
@@ -37,7 +37,7 @@ class CreateTestimoniesTable extends Migration
                     'testimony_studentnaam' => 'Gillian Heyns',
                     'testimony_studierichting' => "3DI",
                     'testimony_jaar' => "2021",
-                    'file_path' => "testURL",
+                    'file_path' => "/uploads/testimonies/Gillian Heyns.jpg",
 //                    'content_block_id' => 1
                 ],
                 [
@@ -47,9 +47,19 @@ class CreateTestimoniesTable extends Migration
                     'testimony_studentnaam' => 'Jonas Van Hove',
                     'testimony_studierichting' => "3DI",
                     'testimony_jaar' => "2021",
-                    'file_path' => "testURL1",
+                    'file_path' => "/uploads/testimonies/Jonas Van Hove.jpg",
 //                    'content_block_id' => 1
-                ]
+                ],
+                [
+                    'created_at' => Carbon::now(),
+                    'updated_at' => Carbon::now(),
+                    'testimony_tekst' => 'ITF Student Axl Kesters',
+                    'testimony_studentnaam' => 'Axl Kesters',
+                    'testimony_studierichting' => "3DI",
+                    'testimony_jaar' => "2021",
+                    'file_path' => "/uploads/testimonies/Axl Kesters.jpg",
+//                    'content_block_id' => 1
+                ],
             ]
         );
     }
