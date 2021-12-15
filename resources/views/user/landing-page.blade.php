86ac40fc67460fa8521d8ab1b8e247f89e0179f8
Jonas Van Hove <r0741524@student.thomasmore.be>
2021-12-15 09:12:38 +0100

    afbeelding tile

--- resources/views/user/landing-page.blade.php
+++ resources/views/user/landing-page.blade.php
@@ -24,7 +24,7 @@
                 </a>
                 <a href="#" style="color: black; text-decoration: none;">
                     <div class="card" style="width: 18rem;">
-                        <img class="card-img-top" src="assets/img/itfactory_interactive/ITF_visual_new.jpg"
+                        <img class="card-img-top" src="assets/img/itfactory_interactive/ITF_robot.jpg"
                              alt="Card image CMS">
                         <div class="card-body">
                             <h5 class="card-title text-bold">DI APP</h5>
